3f461ea4ac156725fbaa3e1c8b66b1dc06f48ad7
danngalann <47415745+danngalann@users.noreply.github.com>
2020-04-27 15:08:19 +0200

    Now using pandas in preprocessing

--- tools/preprocess.py
+++ tools/preprocess.py
@@ -1,49 +1,51 @@
-import csv, math, sys, random
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
-data = {
-    "survived": [],
-    "pClass": [],
-    "sex": [],
-    "age": []
-}
-features, labels = [], []
+"""
+int PassengerId
+int Survived
+int Pclass [1-3]
+string Name
+string Sex (male/female)
+float Age
+int SibSp
+int Parch
+String Ticket
+float Fare
+String Cabin
+char Embarked
+"""
+
+
 
 # Loads data from csv
 def loadData():
-    with open('data.csv', 'r') as csvFile:
-        plots = list(csv.reader(csvFile, delimiter=","))
-        # next(plots, None)  # skip the headers
-        
-        for row in plots[1:100]:            
-            # Is the row missing data?
-            incompleteRow = False
-            for i in range(1,7):
-                if row[i] == '':
-                    incompleteRow = True
+    # Load the data
+    df = pd.read_csv("../data.csv")
 
-            # Skip rows with missing data
-            if not incompleteRow:
-                data["survived"].append(int(row[1]))
-                data["pClass"].append(int(row[2]))
-                data["sex"].append(1 if row[4] == 'male' else 0) # Male = 1, Female = 0
-                data["age"].append(math.floor(float(row[5]))) # Estimated ages come in the format of x.5; floor them.
+    # Drop unused cols
+    unused_cols = ["Ticket", "Fare", "Cabin", "Embarked", "PassengerId", "Name"]
+    df = df.drop(unused_cols, axis=1)
 
-# Imports the data to the features and labels variables
-def importData():
-    for i in range(len(data['pClass'])):
-        features.append([data['pClass'][i], data['age'][i], data['sex'][i]])
-        labels.append(data['survived'][i])
+    # Fill age nulls with the median of the same sex ages
+    df["Age"].fillna(df.groupby("Sex")["Age"].transform("median"), inplace=True)
 
-# Returns training and testing splits of features and labels
-def preprocess():
-    loadData()
-    importData()
-    features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.20)
-    return features_train, features_test, labels_train, labels_test
+    # Map sex to ints
+    sex_mapping = {"male": 0, "female": 1}
+    df["Sex"] = df["Sex"].map(sex_mapping)
+
+    # Convert everything to ints
+    df = df.astype("int")
+
+    # Split data into features and labels
+    labels = pd.DataFrame(df["Survived"])
+    features = df.drop("Survived", axis=1) # Pclass, Sex, Age, SibSp, Parch
+
+    # Return features and labels as a list
+    return features.values.tolist(), labels.values.tolist()
+    
 
 # Returns all the features and labels, without splits
 def getAll():
-    loadData()
-    importData()
+    features, labels = loadData()
     return features, labels
